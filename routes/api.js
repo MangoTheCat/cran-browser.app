c0321749284bdd2614d896550a7b2bc11fc6490d
Gábor Csárdi <csardi.gabor@gmail.com>
2016-06-19 23:19:35 +0100

    Take data from DB
    API is super simple: a GET and a PUT

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,35 @@
 
 var express = require('express');
 var router = express.Router();
+var got = require('got');
+var stream = require('stream');
 
-router.get(new RegExp("^/map/([\\w0-9\\.]+)$"), function(req, res) {
+var db_url = process.env.DOCSDB_URL;
+var pkg_re = new RegExp("^/map/([\\w0-9\\.]+)$");
+
+router.get(pkg_re, function(req, res) {
     var package = req.params[0];
-    res.set('Content-Type', 'application/json')
-	.sendfile(
-	    './public/data/' + package + '.json',
-	    function(err) {
-		if (err) {
-		    console.log(err);
-		    res.status(404).end();
-		}
-	    }
-	);
-})
+    var url = pkg_url(db_url, package);
+    got.stream(url).pipe(res);
+});
+
+router.put(pkg_re, function(req, res) {
+    var package = req.params[0];
+    var auth = db_url.replace(/^http:\/\//, '').replace(/@.*$/, '');
+    var url = pkg_url(db_url, package);
+
+    var s = new stream.Readable();
+    s._read = function noop() {};
+    s.push(JSON.stringify(req.body));
+    s.push(null);
+    s.pipe(got.stream.put(url, { 'auth': auth }));
+
+    res.send('OK');
+});
+
+function pkg_url(db_url, package) {
+    return db_url.replace(/^http:\/\/[^@]*@/, 'http://') +
+	'/code/' + package;
+}
 
 module.exports = router;
